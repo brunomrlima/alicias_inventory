d51568f5c366e04a8230c57afad828adabd11de3
brunomrlima <bruno.mrlima@gmail.com>
2019-07-04 16:44:56 -0400

    adding fulltittle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  private
+
+  def full_title(page_title = '')
+    base_title = "Baja Vibora"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
